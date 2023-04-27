ff54e70f9e489b78fe2183052b2a8a1bcafda61b
sakoguchi <sakoguchi-yusei1018@g.ecc.u-tokyo.ac.jp>
2023-04-27 14:10:53 +0900

    task01 finished

--- task01/main.cpp
+++ task01/main.cpp
@@ -40,8 +40,8 @@ Eigen::Vector2f time_integration_implicit(const Eigen::Vector2f& p0, float dt){
   Eigen::Matrix2f A;
   Eigen::Vector2f b;
   // modify the following two lines to implement implicit time integration
-  A << 1.f, 0.f, 0.f, 1.f;
-  b << r0, v0;
+  A << 1.f, -dt, 0, 1.f-dt*dt*dfdr;
+  b << r0, v0+dt*f0;
   return A.inverse()*b;
 }
 
